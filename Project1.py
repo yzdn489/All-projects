37d7dcb20cce08d186412982a67ad602f385fc2a
yzdn489 <81864795+yzdn489@users.noreply.github.com>
2021-04-03 20:13:30 +0430

    Add files via upload

---
+++ Project1.py
@@ -0,0 +1,8 @@
+name = input("Please enter your name:")
+print("Welcome",name,"!")
+age = int(input("Please enter your age-->"))
+if age >= 18:
+       print("hi" ,name, "mr/ms")
+if age < 18:
+       print("hi" ,name, "your age less of 18 old") 
+  
